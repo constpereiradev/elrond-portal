ac9630d70fd9003d2469c0bb2163272d111f6296
constpereiradev <amandapereiradevcontact@gmail.com>
2026-02-24 08:29:12 -0300

    feat: Testes de Council

---
+++ tests/Feature/CouncilTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\RoleEnum;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class CouncilTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Role::create([
+            'name' => 'Membro',
+            'slug' => RoleEnum::membro->value
+        ]);
+
+        Role::create([
+            'name' => 'Administrador',
+            'slug' => RoleEnum::admin->value
+        ]);
+    }
+
+    /** @test */
+    public function non_admin_cannot_create_council()
+    {
+        /** @var User $commonUser */
+        $commonUser = User::factory()->create([
+            'role_id' => Role::where('slug', RoleEnum::membro->value)->first()->id
+        ]);
+
+        $payload = [
+            'name' => 'Test Council',
+        ];
+
+        $this->actingAs($commonUser)
+            ->postJson('api/v1/council', $payload)->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    /** @test */
+    public function admin_can_create_council()
+    {
+        /** @var User $commonUser */
+        $commonUser = User::factory()->create([
+            'role_id' => Role::where('slug', RoleEnum::admin->value)->first()->id
+        ]);
+
+        $payload = [
+            'name' => 'Test Council',
+        ];
+
+        $this->actingAs($commonUser)
+            ->postJson('api/v1/council', $payload)->assertStatus(Response::HTTP_OK);
+    }
+}
