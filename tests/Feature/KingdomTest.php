41c0f279fb833637b7e20175e117b79654245b07
constpereiradev <amandapereiradevcontact@gmail.com>
2026-02-24 08:36:08 -0300

    feat: Kingdom testes

---
+++ tests/Feature/KingdomTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\RoleEnum;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class KingdomTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Role::create([
+            'name' => 'Membro',
+            'slug' => RoleEnum::membro->value
+        ]);
+
+        Role::create([
+            'name' => 'Administrador',
+            'slug' => RoleEnum::admin->value
+        ]);
+
+        Role::create([
+            'name' => 'Conselho',
+            'slug' => RoleEnum::conselho->value
+        ]);
+    }
+
+    public function test_non_admin_cannot_create_kingdom()
+    {
+        /** @var User $commonUser */
+        $commonUser = User::factory()->create([
+            'role_id' => Role::where('slug', RoleEnum::membro->value)->first()->id
+        ]);
+
+        $payload = [
+            'name' => 'Test Kingdom',
+        ];
+
+        $this->actingAs($commonUser)
+            ->postJson('api/v1/kingdom', $payload)->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    public function test_admin_can_create_kingdom()
+    {
+        /** @var User $commonUser */
+        $commonUser = User::factory()->create([
+            'role_id' => Role::where('slug', RoleEnum::admin->value)->first()->id
+        ]);
+
+        $payload = [
+            'name' => 'Test Kingdom',
+        ];
+
+        $this->actingAs($commonUser)
+            ->postJson('api/v1/kingdom', $payload)->assertStatus(Response::HTTP_OK);
+    }
+}
