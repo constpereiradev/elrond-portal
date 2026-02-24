668f8ef24603dcd47096a01926b077865cbbfbe6
constpereiradev <amandapereiradevcontact@gmail.com>
2026-02-24 09:40:09 -0300

    feat: Swagger doc

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,17 +7,20 @@ use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
-        apiPrefix: 'api/v1',    
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
+        apiPrefix: 'api/v1',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
         $middleware->prependToGroup('api', [
             HandlePrecognitiveRequests::class,
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            'api/*',  
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
